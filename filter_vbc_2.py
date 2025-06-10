8c09790940f7b949cf30e1d9e22ab34dade9e3a5
kitt-cellecta <kparaiso@cellecta.com>
2025-06-10 14:26:00 -0700

    modified selection of minimas when there is only one maxima

--- filter_vbc_2.py
+++ filter_vbc_2.py
@@ -11,72 +11,6 @@ from scipy.signal import find_peaks, argrelextrema
 from sklearn.neighbors import KernelDensity
 from scipy.stats import iqr
 
-def find_gmm_threshold(data):
-    """Finds a threshold using Gaussian Mixture Model (GMM)."""
-    if len(data) < 2:
-        return None
-
-    data_array = np.log10(data[data > 0].values).reshape(-1, 1)
-
-    gmm = GaussianMixture(n_components=2, random_state=42)
-    gmm.fit(data_array)
-
-    mean1, mean2 = gmm.means_.flatten()
-    var1, var2 = gmm.covariances_.flatten()
-    weight1, weight2 = gmm.weights_.flatten()
-
-    if mean1 > mean2:
-        mean1, mean2 = mean2, mean1
-        var1, var2 = var2, var1
-        weight1, weight2 = weight2, weight1
-
-    a = 1 / (2 * var1) - 1 / (2 * var2)
-    b = mean2 / var2 - mean1 / var1
-    c = mean1**2 / (2 * var1) - mean2**2 / (2 * var2) + np.log(var2 / var1)
-    intersection = (-b + np.sqrt(b**2 - 4*a*c)) / (2*a)
-
-    return 10**intersection
-    
-    # Alternative solution: Minimum point detection method
-    #def gaussian_mixture(x):
-    #    return -1 * (weight1 * norm.pdf(x, mean1, np.sqrt(var1)) +
-    #                 weight2 * norm.pdf(x, mean2, np.sqrt(var2)))
-    #                 
-    #result = minimize_scalar(gaussian_mixture, bracket=(mean1, mean2))
-    #threshold = result.x
-    #
-    #return 10**threshold
-    
-def find_kde_mimima_threshold(data):
-    """Finds threshold using KDE minima detection."""
-    if len(data) < 2:
-        return None
-    
-    data_array = np.log10(data[data > 0].values).reshape(-1, 1)
-    
-    # Adaptive bandwidth selection
-    bandwidth = 0.5 * data_array.std()  # Silverman's rule of thumb factor
-    if bandwidth == 0 or np.isnan(bandwidth):
-        return None
-    
-    # Perform KDE analysis
-    kde = KernelDensity(bandwidth=bandwidth)
-    kde.fit(data_array)
-    x = np.linspace(data_array.min(), data_array.max(), 1000).reshape(-1, 1)
-    log_dens = kde.score_samples(x)
-    
-    # find locations of minima
-    minima = argrelextrema(log_dens, np.less)[0]
-    if len(minima) == 0:
-        return None
-    
-    # Find first major minimum (ignore small fluctuations)
-    main_minima = minima[log_dens[minima] < np.percentile(log_dens, 25)]
-    if len(main_minima) == 0:
-        return 10**x[minima[0]][0]  # Fallback to first minimum
-    
-    return 10**x[main_minima[0]][0]
-
 def find_kde_mimima_threshold_2(data, barcode_count, output_prefix):
     """Finds threshold using KDE minima detection between two highest maxima."""
     if len(data) < 2:
@@ -107,11 +41,15 @@ def find_kde_mimima_threshold_2(data, barcode_count, output_prefix):
         return (2, 0, 0)
     
     if len(maxima) == 1:
-        left_max_constant = 0 # setting the first peak as non-existent at 0
+        left_max = 0 # setting the first peak as non-existent at 0
         right_max = maxima[0]
 		
-		# Return fixed value as the cutoff if there's only one peak
-        return (2, left_max_constant, 10**x[right_max][0])
+        # Find minima between these two maxima
+        between_minima = minima[(minima > left_max) & (minima < right_max)]
+        if between_minima.size > 0:
+            # Select the most prominent minimum (lowest log density)
+            selected_min = between_minima[np.argmin(log_dens[between_minima])]
+            return (10**x[selected_min][0], 10**x[left_max][0], 10**x[right_max][0])
             
     if len(maxima) >= 2:
         # Get two highest maxima (by log density)
@@ -161,10 +99,10 @@ def main(input_file, output_prefix):
     for i in range(1, len(sorted_keys)):
         current_key = sorted_keys[i]
         previous_key = sorted_keys[i - 1]
-        #if thresholds[current_key] > 10 * thresholds[previous_key]:
-        #    right_maxes[current_key] = left_maxes[current_key] 	# the first peak is actually the second peak
-        #    left_maxes[current_key] = 0							# the first peak is set at 0
-        #    thresholds[current_key] = 2							# default cutoff value = 2
+        if thresholds[current_key] > 10 * thresholds[previous_key]:
+            right_maxes[current_key] = left_maxes[current_key] 	# the first peak is actually the second peak
+            left_maxes[current_key] = 0							# the first peak is set at 0
+            thresholds[current_key] = 2							# default cutoff value = 2
             
 	# Save maxima and threshold locations to a file
     with open(f"{output_prefix}.kde.maximas.txt", "a") as f:
